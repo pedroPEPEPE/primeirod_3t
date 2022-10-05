f8f6bd6effbc9513798114fcf576fed308ca7c2d
pedroPEPEPE <110109247+pedroPEPEPE@users.noreply.github.com>
2022-10-05 11:09:56 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,1 +1,28 @@
+let tabuada = 2;
 
+function escreva(){
+    document.write(tabuada + " x 1 = "+(tabuada*1)+"<br>");
+    document.write(tabuada + " x 2 = "+(tabuada*2)+"<br>");
+    document.write(tabuada + " x 3 = "+(tabuada*3)+"<br>");
+    document.write(tabuada + " x 4 = "+(tabuada*4)+"<br>");
+    document.write(tabuada + " x 5 = "+(tabuada*5)+"<br>");
+    document.write(tabuada + " x 6 = "+(tabuada*6)+"<br>");
+    document.write(tabuada + " x 7 = "+(tabuada*7)+"<br>");
+    document.write(tabuada + " x 8 = "+(tabuada*8)+"<br>");
+    document.write(tabuada + " x 9 = "+(tabuada*9)+"<br>");
+    document.write(tabuada + " x 10 = "+(tabuada*10)+"<br>");
+}
+function repete(){
+    for(let i = 0; i < 5; i++){
+        document.write()
+        for(let j =1; j <=10; j++){
+
+        }
+        document.write(i + " x " + i + " = "+(j*i)+"<br"
+    }
+    document.write("<br>");
+}
+let frutas= ["abacaxi HAHAHAHA"
+function minhaLista(){
+    document.write(Lista)
+}
